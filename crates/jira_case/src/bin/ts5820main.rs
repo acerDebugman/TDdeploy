3c2ef8dde773ed1f9225c8a55ea91913a684873d
acerDebugman <460873250@qq.com>
2025-09-24 18:23:44 +0800

    wip

--- crates/jira_case/src/bin/ts5820main.rs
+++ crates/jira_case/src/bin/ts5820main.rs
@@ -1,7 +1,7 @@
-use jira_case::{gen_data::td_blob_data};
+use jira_case::gen_data::{td_blob_data_big, td_blob_data_small};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    td_blob_data().await?;
+    td_blob_data_big().await?;
     Ok(())
 }
