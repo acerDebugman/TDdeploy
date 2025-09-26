f49d329120495506a5df34d1e785019398b975c6
acerDebugman <460873250@qq.com>
2025-09-26 15:32:14 +0800

    wip

--- crates/jira_case/src/bin/ts5820main.rs
+++ crates/jira_case/src/bin/ts5820main.rs
@@ -3,8 +3,8 @@ use jira_case::{gen_data::{td_blob_data_big, td_blob_data_small}, ts5820::{kafka
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // td_blob_data_big().await?;
-    // test_tmq().await?;
+    test_tmq().await?;
     // kafka_main().await?;
-    subscribe::subscribe().await?;
+    // subscribe::subscribe().await?;
     Ok(())
 }
