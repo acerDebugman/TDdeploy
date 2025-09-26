f939d1b1aea638d3463398dac6fd2a025b44a1bd
acerDebugman <460873250@qq.com>
2025-09-26 16:34:36 +0800

    wip

--- crates/jira_case/src/ts5820/mod.rs
+++ crates/jira_case/src/ts5820/mod.rs
@@ -45,8 +45,8 @@ pub async fn loop_producer(dsn: &str, db: &str, limit: usize) -> anyhow::Result<
         // 3M = 20 * 1000 * 150
         // let item = format!("\\x{}", "255044462D312E330D0A".repeat(1000 * 150));
         // 500k = 20 * 1000 * 25
-        // let item = format!("\\x{}", "255044462D312E330D0A".repeat(1000 * 25));
-        let item = format!("\\x{}", "255044462D312E330D0A");
+        let item = format!("\\x{}", "255044462D312E330D0A".repeat(1000 * 25));
+        // let item = format!("\\x{}", "255044462D312E330D0A");
         println!("item len: {}", item.len());
         let inserted = taos.exec_many([
             &format!("INSERT INTO `t1` using `meters` (`groupid`,`location`) tags(1,\"BJ\") (`ts`,`id`,`voltage`,`v_blob`) values(now,1,11,'{}')", item),
