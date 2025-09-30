c9b79a07aedf15cfcc64ce567855624cf2a6d663
acerDebugman <460873250@qq.com>
2025-09-30 17:51:26 +0800

    wip

--- crates/jira_case/src/main.rs
+++ crates/jira_case/src/main.rs
@@ -1,10 +1,7 @@
-mod offen;
-use jira_case::offen;
-
 
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    offen::mysql::mysql_main().await?;
+    jira_case::offen::mysql::mysql_main().await?;
     Ok(())
 }
