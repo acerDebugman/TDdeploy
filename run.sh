a1c7944b69c06e3b4f466e56f0913e26c15068ba
acerDebugman <460873250@qq.com>
2025-09-30 17:52:40 +0800

    wip

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@
 git pull
 
 #cargo run -p jira_case --bin ts5820main
-#cargo run -p jira_case --bin jira_case
-cargo run --package jira_case --bin jira_case
+cargo run -p jira_case --bin jira_case
 
