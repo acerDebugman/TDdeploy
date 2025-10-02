7abef032016cdbe9d0ee5cae48efa86fd25a3e43
guichuan zhang <460873250@qq.com>
2025-10-02 22:51:58 +0800

    wip

--- coding_tips/build/CMakeFiles/thread_in_turn.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for thread_in_turn.
-# This may be replaced when dependencies are built.
--- coding_tips/build/CMakeFiles/thread_in_turn.dir/thread_in_turn/thread_in_turn.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/thread_in_turn.dir/thread_in_turn/thread_in_turn.c.o: \
- /home/algo/rust_space/TDdeploy/coding_tips/thread_in_turn/thread_in_turn.c \
- /usr/include/stdc-predef.h
