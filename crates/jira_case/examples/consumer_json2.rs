0dcda76ec0856020b4d269c2b3a0a6aa2ac49caa
acerDebugman <460873250@qq.com>
2025-10-24 16:34:16 +0800

    wip

--- crates/jira_case/examples/consumer_json2.rs
+++ crates/jira_case/examples/consumer_json2.rs
@@ -96,6 +96,7 @@ async fn main() -> Result<(), pulsar::Error> {
     let last_msg_id = consumer.get_last_message_id().await?;
     log::info!("xxxzgc last_msg_id: {:?}", last_msg_id);
 
+    // 使用seek， 在 failover 模式下，会让其他 consumer 的 connection 重连，进而到指定位置读取
     // let earliest_id_data = MessageIdData {
     //     ledger_id: u64::MAX,
     //     entry_id: u64::MAX,
@@ -103,13 +104,13 @@ async fn main() -> Result<(), pulsar::Error> {
     // };
     // consumer.seek(Some(consumer.topics()), Some(earliest_id_data.clone()), None, pulsar).await?;
     // log::info!("seek to earliest_id_data: {:?}", earliest_id_data);
-    // let latest_id_data = MessageIdData {
-    //     ledger_id: u64::MAX,
-    //     entry_id: u64::MAX,
-    //     ..Default::default()
-    // };
-    // consumer.seek(Some(consumer.topics()), Some(latest_id_data), None, pulsar).await?;
-    // log::info!("seek to latest_id_data: {:?}", latest_id_data);
+    let latest_id_data = MessageIdData {
+        ledger_id: u64::MAX,
+        entry_id: u64::MAX,
+        ..Default::default()
+    };
+    consumer.seek(Some(consumer.topics()), Some(latest_id_data.clone()), None, pulsar).await?;
+    log::info!("seek to latest_id_data: {:?}", latest_id_data);
     // let msg_id_data = MessageIdData {
     //     ledger_id: 5,
     //     entry_id: 2,
@@ -117,9 +118,10 @@ async fn main() -> Result<(), pulsar::Error> {
     // };
     // consumer.seek(Some(consumer.topics()), Some(msg_id_data.clone()), None, pulsar).await?;
     // log::info!("seek to msg_id_data: {:?}", msg_id_data);
-    let now_ts = chrono::Utc::now().timestamp_millis() as u64;
-    consumer.seek(Some(consumer.topics()), None, Some(now_ts), pulsar).await?;
-    log::info!("seek to timestamp now_ts: {:?}", now_ts);
+
+    // let now_ts = chrono::Utc::now().timestamp_millis() as u64;
+    // consumer.seek(Some(consumer.topics()), None, Some(now_ts), pulsar).await?;
+    // log::info!("seek to timestamp now_ts: {:?}", now_ts);
 
     let mut counter = 0usize;
     while let Some(msg) = consumer.try_next().await? {
