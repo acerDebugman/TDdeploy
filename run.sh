aa386ef2b9c42d559b19f0b18ee71b4132a47a00
acerDebugman <460873250@qq.com>
2025-11-04 18:43:25 +0800

    wip

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ git pull
 
 #cargo run -p jira_case --bin ts5820main
 
-RUST_LOG=info cargo run -p jira_case --bin jira_case
+#RUST_LOG=info cargo run -p jira_case --bin jira_case
 
 # example
 #RUST_LOG=info cargo run -p jira_case --example consumer_json
 #RUST_LOG=info cargo run -p jira_case --example producer_json_loop
 # for mqtt
 #cargo run -p test_rust_conn --bin flashmq_pub_taosx
+# for tuya
+RUST_LOG=info cargo run -p jira_case --example pulsar_producer_tuya_loop
