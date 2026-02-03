7149adee2618b584866cc95a0994bc6517309f77
acerDebugman <460873250@qq.com>
2026-02-03 16:10:03 +0800

    wip

--- crates/jira_case/src/ts5820/kafka.rs
+++ crates/jira_case/src/ts5820/kafka.rs
@@ -13,7 +13,7 @@ pub async fn kafka_main() -> anyhow::Result<()> {
     // let kafka_addr = "172.20.0.3:9092"; // 替换为你的Kafka地址
     // let kafka_addr = "172.20.0.9:9092"; // 替换为你的Kafka地址
     // let kafka_addr = "172.19.0.9:9092"; // 替换为你的Kafka地址
-    let kafka_addr = "192.168.2.131:9092"; // 替换为你的Kafka地址
+    let kafka_addr = "192.168.2.158:9092"; // 替换为你的Kafka地址
     let topic = "test-topic"; // 替换为你的topic
     // let topic = "test-topic3"; // 替换为你的topic
     let duration = Duration::from_secs(3600000); // 持续10秒
